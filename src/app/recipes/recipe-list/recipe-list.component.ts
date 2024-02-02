6fb367099ba2826ff7c91d7dd05d34ff62cccee1
aamirindi <aamirindi@gmail.com>
2024-02-02 16:11:16 +0530

    updated

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe("A test Recipe", 'this is simply a test', 'https://assets.epicurious.com/photos/6508a73d04e0d5ae4ef86c6e/16:9/w_5455,h_3068,c_limit/Dauphinoise-Potatoes_RECIPE.jpg')
+    new Recipe("A test Recipe", 'This is simply a test', 'https://assets.epicurious.com/photos/6508a73d04e0d5ae4ef86c6e/16:9/w_5455,h_3068,c_limit/Dauphinoise-Potatoes_RECIPE.jpg'),
+    new Recipe("A test Recipe", 'This is simply a test', 'https://assets.epicurious.com/photos/6508a73d04e0d5ae4ef86c6e/16:9/w_5455,h_3068,c_limit/Dauphinoise-Potatoes_RECIPE.jpg'),
   ];
 }
